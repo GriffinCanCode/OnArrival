7f2c7bd9a84f1d2c1004eba0f60ee1aaa4982fa8
greyhatharold <gjstrier@gmail.com>
2024-11-08 01:24:44 -0500

    2

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,13 +1,14 @@
 import sys
 import os
 
-# Add your project directory to Python path
-path = '/home/griffinstrier/projects/onarrival'
-if path not in sys.path:
-    sys.path.append(path)
+# Add the src directory to the Python path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
 
 # Set environment variable
 os.environ['PYTHONANYWHERE_DOMAIN'] = 'griffinstrier.pythonanywhere.com'
 
 # Import your Flask app
-from web_app import app as application 
+from src.web_app import app as application
